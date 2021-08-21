d583e9f3a9bffe49fdc23ef3c65b89d28f3eb03d
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-08-21 08:49:33 +0200

    improve book library styling

--- app/blueprints/projects/cog/apps/book_library/__init__.py
+++ app/blueprints/projects/cog/apps/book_library/__init__.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 
 bp = Blueprint(name="book_library",
                import_name=__name__,
-               template_folder="./templates")
+               template_folder="./templates",
+               static_folder="static")
 
 from app.blueprints.projects.cog.apps.book_library import routes
